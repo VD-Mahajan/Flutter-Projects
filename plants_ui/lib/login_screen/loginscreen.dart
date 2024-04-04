ca15657907785a2d96365687aa64d32245af8c78
Vishal Datta Mahajan <mahajanvishal410a@gmail.com>
2024-04-04 22:57:11 +0530

    login screen completed

--- plants_ui/lib/login_screen/loginscreen.dart
+++ plants_ui/lib/login_screen/loginscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,7 +13,77 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [Image.asset('assets/circle.png')],
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Transform.translate(
+                offset: const Offset(-29, -10),
+                child: Image.asset(
+                  'assets/circle.png',
+                  width: 210,
+                  height: 128,
+                ),
+              ),
+            ],
+          ),
+          const Text(
+            'Log in',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.phone_outlined),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                hintText: 'Mobile Number',
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width * 1,
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromRGBO(124, 180, 70, 1),
+                    Color.fromRGBO(62, 102, 24, 1),
+                  ],
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Text(
+                  'Log in',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Image.asset('assets/image2.png')
+        ],
       ),
     );
   }
